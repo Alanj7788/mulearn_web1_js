0d26707351e47b52f34c25c93104aeb44d1bb8b9
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2023-11-17 21:56:16 +0530

    Add files via upload

---
+++ web1.js
@@ -0,0 +1,6 @@
+document.getElementById("rollButton").addEventListener("click", rollDice);
+
+function rollDice() {
+    var diceRoll = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("result").innerText = 'You got ' + diceRoll;
+}
